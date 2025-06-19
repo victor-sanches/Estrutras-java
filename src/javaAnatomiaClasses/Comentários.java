448e4cbd7ec16700e8691b0cc3a2801e5b93d6a2
Victor-Sanhces <joaovictorsanches0902@gmail.com>
2025-06-19 11:00:16 -0400

    Primeiro commit do projeto Java

---
+++ src/javaAnatomiaClasses/Comentários.java
@@ -0,0 +1,22 @@
+package javaAnatomiaClasses;
+
+public class Comentários {
+    public static void main(String[] args) {
+        // Olá, eu sou um comentário em uma unica linha
+
+        /*
+         * Eu sou um comentário
+         * que posso ser mais detalhado
+         * quando necessário
+         */
+
+        /**
+         * Estas duas estrelas acima
+         * é para identificar que você
+         * prentende elaborar um comentário
+         * a nível de documentação.
+         * Que incrivel !!!
+         */
+
+    }
+}
